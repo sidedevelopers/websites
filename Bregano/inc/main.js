f0d55193be6ae877d9da66c577cec90c1e506cb0
sidedevelopers <56724326+sidedevelopers@users.noreply.github.com>
2025-04-12 04:45:18 +0000

    updated

--- Bregano/inc/main.js
+++ Bregano/inc/main.js
@@ -15,3 +15,29 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 // sticky navbar end
+
+$(".hp-carousel").owlCarousel({
+    autoplay: true,
+    items: 3,
+    dots: false,
+    loop: true,
+    nav: true,
+    margin: 20,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        485: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        960: {
+            items: 3,
+        },
+        1200: {
+            items: 3,
+        },
+    },
+});
