1e685a4eca012375a5393fb8bbb43beea6b35223
sidedevelopers <56724326+sidedevelopers@users.noreply.github.com>
2025-09-12 17:25:29 +0000

    updated

--- Aroma-Vista-UAE/inc/main.js
+++ Aroma-Vista-UAE/inc/main.js
@@ -8,6 +8,7 @@ const swiper = new Swiper('.hero-swiper', {
     keyboard: {
         enabled: true,
     },
+    grabCursor: true,
 
     // If we need pagination
     pagination: {
@@ -26,6 +27,45 @@ const swiper = new Swiper('.hero-swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
+
+
+const swiper2 = new Swiper('.products-swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    keyboard: {
+        enabled: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    // },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
 
     // And if we need scrollbar
     // scrollbar: {
